a9d61b88f2f37356251335d57e89381fda8f87f0
colombohenrique <jhbcolombo@gmail.com>
2022-02-04 00:36:37 -0300

    Finishing chapter 2

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/7 - Enums/depois/Enumeracoes.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/7 - Enums/depois/Enumeracoes.cs
@@ -10,6 +10,25 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
+            //const int Seg = 0;
+            //const int Ter = 1;
+            //const int Qua = 2;
+            //const int Qui = 3;
+            //const int Sex = 4;
+            //const int Sab = 5;
+            //const int Dom = 6;
+
+            DiasDaSemana primeiroDia = DiasDaSemana.Seg;
+
+            DiasDeTrabalho diasDeTrabalho = DiasDeTrabalho.Ter | DiasDeTrabalho.Qui | DiasDeTrabalho.Sex;
+
+            Console.WriteLine(diasDeTrabalho);
         }
     }
+
+    enum DiasDaSemana {Seg = 3, Ter = 10, Qua = 15, Qui, Sex, Sab, Dom}
+
+    [Flags]
+    enum DiasDeTrabalho { Seg = 0, Ter = 1, Qua = 2, Qui = 4, Sex = 8, Sab = 16, Dom = 32 }
+
 }
